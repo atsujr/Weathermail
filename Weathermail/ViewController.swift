2e9dbaaeecaed277a54873bbf0f645717fc44a68
Atsuhiro Muroyama <atuhiro.duda@gmail.com>
2022-05-22 11:24:50 +0900

    Initial Commit

---
+++ Weathermail/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weathermail
+//
+//  Created by Atsuhiro Muroyama on 2022/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
